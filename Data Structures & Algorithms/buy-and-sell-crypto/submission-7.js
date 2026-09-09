66c257d0280144fb3fdc5798bb8e329d8a887a6d
Chi Binh Vuong <162963809+binhvuong1772003@users.noreply.github.com>
2026-09-09 23:01:40 +0700

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let buy = prices[0], profit = 0;
+
+        for (let i = 1; i < prices.length; i++) {
+            buy = Math.min(buy, prices[i]);
+            profit = Math.max(profit, prices[i] - buy);
+        }
+        return profit;
+    }
+}
