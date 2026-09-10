d3b9c8018fb81a07356dabc4082e2820a3256711
Chi Binh Vuong <162963809+binhvuong1772003@users.noreply.github.com>
2026-09-11 00:06:24 +0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,41 @@
+var MinStack = function() {
+    this.stack = [];
+    this.minStack = [];
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack.push(val);
+
+    if (this.minStack.length === 0) {
+        this.minStack.push(val);
+    } else {
+        const currentMin = this.minStack[this.minStack.length - 1];
+        this.minStack.push(Math.min(val, currentMin));
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.stack.pop();
+    this.minStack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.minStack[this.minStack.length - 1];
+};
