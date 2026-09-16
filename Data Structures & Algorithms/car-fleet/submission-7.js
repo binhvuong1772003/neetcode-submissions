d9c8f4565d95fd53f2625d80c16ed70132f66eff
Chi Binh Vuong <162963809+binhvuong1772003@users.noreply.github.com>
2026-09-17 01:50:52 +0700

    Add: car-fleet - submission-7

---
+++ Data Structures & Algorithms/car-fleet/submission-7.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        const cars = []
+        for (let i =0; i< position.length;i++){
+            cars.push([position[i],speed[i]])
+        }
+        cars.sort((a, b) => b[0] - a[0]);
+        const stack = []
+        let fleet = 0
+        for (const[pos,spd] of cars){
+            const time = (target - pos)/spd
+            if (stack.length===0) {
+                stack.push(time)
+                fleet+=1
+            }
+            if(stack.at(-1) < time){
+                fleet+=1
+                stack.push(time)
+            }
+        }
+        return fleet
+    }
+}
